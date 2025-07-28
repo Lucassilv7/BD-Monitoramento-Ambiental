eab16f04855a4aa67b54c28d42f59934b79e5cb2
Lucas Silva de Souza <lucassilv0111@gmail.com>
2025-07-28 07:20:08 -0300

    Log

--- src/log/Logger.java
+++ src/log/Logger.java
@@ -1,10 +1,13 @@
 package log;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.StringWriter;
 
 public class Logger {
 
-    private BufferedWriter writer;
+    private BufferedWriter writer, writerMemoria;
+    private StringWriter stringWriter;;
 
     /**
      * Construtor da classe Logger.
@@ -13,6 +16,8 @@ public class Logger {
     public Logger(String nomeArquivo){
         try {
             this.writer = new BufferedWriter(new java.io.FileWriter(nomeArquivo, true));
+            stringWriter = new StringWriter();
+            writerMemoria = new BufferedWriter(stringWriter);
         } catch (Exception e) {
             System.out.println("Erro ao abrir o arquivo de log: " + e.getMessage());
         }
@@ -24,8 +29,11 @@ public class Logger {
     public void registrar(String mensagem){
         try {
             writer.write("[" + java.time.LocalDateTime.now() + "] " + mensagem);
+            writerMemoria.write("[" + java.time.LocalDateTime.now() + "] " + mensagem);
             writer.newLine();
+            writerMemoria.newLine();
             writer.flush();
+            writerMemoria.flush();
         } catch (Exception e) {
             System.out.println("Erro ao registrar no log: " + e.getMessage());
         }
@@ -36,8 +44,23 @@ public class Logger {
     public void fechar() {
         try {
             writer.close();
+            writerMemoria.close();
+            stringWriter.close();
         } catch (Exception e) {
             System.out.println("Erro ao fechar o arquivo de log: " + e.getMessage());
         }
     }
+    /**
+     * Método para obter o conteúdo do log em memória.
+     * @return Conteúdo do log em memória.
+     */
+    public String getLogEmMemoria() {
+        try {
+            writerMemoria.flush();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return stringWriter.toString();
+    }
+
 }
